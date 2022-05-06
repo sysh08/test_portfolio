ee881a0d7bdccbdce799123d59e01439a9b47b36
testkhp <seoyun5001@naver.com>
2022-05-06 14:25:58 +0900

    포트폴리오 메인파일 및 css js 코드 수정

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 let test = document.querySelector(".test")
+let test2 = document.querySelector("span")
+
+test.addEventListener("click",function(){
+    alert("경고창이 떠요");
+});
+
+test2.addEventListener("mouseenter",function(){
+    alert("마우스를 올렸네");
+});
